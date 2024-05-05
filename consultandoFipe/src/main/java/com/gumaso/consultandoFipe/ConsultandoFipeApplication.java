4de85870abce05b683f9b182c6d824b63911dfc5
Gustavo <gustavomacedosousa013@gmail.com>
2024-05-05 12:52:51 -0300

    Criando o nosso projeto e os arquivos base

---
+++ consultandoFipe/src/main/java/com/gumaso/consultandoFipe/ConsultandoFipeApplication.java
@@ -0,0 +1,13 @@
+package com.gumaso.consultandoFipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultandoFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultandoFipeApplication.class, args);
+	}
+
+}
---
+++ consultandoFipe/src/test/java/com/gumaso/consultandoFipe/ConsultandoFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumaso.consultandoFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultandoFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
